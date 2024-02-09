840199d39a02d4cd72a34f173ce7ef9640f18208
mkhotamirais <mkhotamirais@gmail.com>
2024-02-09 19:04:58 +0700

    finish

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -15,8 +15,8 @@ const Header = () => {
   };
 
   return (
-    <header className="h-16 border-b px-3 lg:px-16 flex items-center justify-between sticky top-0 backdrop-blur backdrop-saturate-100 bg-[rgba(255,255,255,0.5)]">
-      <a href="#" className="font-semibold text-xl uppercase">
+    <header className="z-50 h-16 border-b px-3 lg:px-16 flex items-center justify-between sticky top-0 backdrop-blur backdrop-saturate-100 bg-[rgba(255,255,255,0.5)]">
+      <a href="https://portfolio-vercel" className="font-semibold text-xl uppercase">
         mkhotami
       </a>
       <div className="flex gap-2 sm:gap-4 lg:gap-10">
--- src/pages/project/Project.jsx
+++ src/pages/project/Project.jsx
@@ -1,9 +1,42 @@
 import { H1, Section } from "../../components/Tags";
 
+const projectsList = [
+  { name: "Eduwork Fe Sisko", imagePath: "images/eduwork-fesisko.jpg", url: "https://eduwork-fesisko.vercel.app/" },
+  { name: "Hadinalmusri", imagePath: "images/hadinalmusri.jpg", url: "https://hadinalmusri.com" },
+  {
+    name: "Nurul Iman",
+    imagePath: "images/manuruliman.jpg",
+    url: "https://mkhotamirais.github.io/simple-webpage-nuruliman/",
+  },
+  { name: "WPU Bootstrap", imagePath: "images/wpu-bs1.jpg", url: "https://mkhotamirais.github.io/simple-webpage-wpu-bs1/" },
+  {
+    name: "WPU Materialize",
+    imagePath: "images/wpu-mtrz1.jpg",
+    url: "https://mkhotamirais.github.io/simple-webpage-wpu-mtrz1/",
+  },
+  { name: "MrWebdesigner", imagePath: "images/mrwd1.jpg", url: "https://mkhotamirais.github.io/simple-webpage-mrwd1/" },
+];
+
 const Project = () => {
   return (
     <Section id="projects">
-      <H1>Project</H1>
+      <H1>Projects</H1>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {projectsList.map((pl) => (
+          <a
+            key={pl.name}
+            href={pl.url}
+            target="blank"
+            rel="noopener"
+            className="bg-white p-2 rounded hover:shadow-xl group transition-all duration-200"
+          >
+            <figure className="overflow-hidden">
+              <img src={pl.imagePath} alt={pl.name} className="group-hover:scale-110 transition-all duration-200" />
+            </figure>
+            <h3 className="font-semibold text-center leading-loose group-hover:text-blue-500">{pl.name}</h3>
+          </a>
+        ))}
+      </div>
     </Section>
   );
 };
